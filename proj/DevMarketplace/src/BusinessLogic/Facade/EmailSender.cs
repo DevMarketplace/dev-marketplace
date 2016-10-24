1a603b67ab2d81a5371b5dcfc886886d061da640
Tosho Toshev <tosho.toshev@emerson.com>
2016-10-24 13:02:38 -0400

    EmailSender implementation Work In Progress

--- proj/DevMarketplace/src/BusinessLogic/Facade/EmailSender.cs
+++ proj/DevMarketplace/src/BusinessLogic/Facade/EmailSender.cs
@@ -1,8 +1,6 @@
 ﻿using MailKit.Net.Smtp;
-using MailKit.Security;
 using MimeKit;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,27 +8,63 @@ namespace BusinessLogic.Facade
 {
     public class EmailSender : IEmailSender
     {
-        public async Task SendTextEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(EmailSenderConfiguration configuration)
         {
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("Joe Bloggs", "jbloggs@example.com"));
-            emailMessage.To.Add(new MailboxAddress("", email));
-            emailMessage.Subject = subject;
-            emailMessage.Body = new TextPart("plain") { Text = message };
+            emailMessage.From.Add(new MailboxAddress(configuration.From.Name, configuration.From.EmailAddress));
+            if (!SetRecipients(configuration, emailMessage))
+            {
+                throw new InvalidOperationException("There are no recipients set.");
+            }
+
+            SetCarbonCopy(configuration, emailMessage);
+
+            emailMessage.Subject = configuration.To.Subject;
+            emailMessage.Body = new TextPart(configuration.EmailFormat) { Text = configuration.EmailBody };
 
             using (var client = new SmtpClient())
             {
-                client.LocalDomain = "some.domain.com";
-                await client.ConnectAsync("smtp.relay.uri", 25, SecureSocketOptions.None).ConfigureAwait(false);
+                client.LocalDomain = configuration.Domain;
+                await client.ConnectAsync(configuration.SmtpServer, configuration.SmtpPort, configuration.SecureSocketOption).ConfigureAwait(false);
                 await client.SendAsync(emailMessage).ConfigureAwait(false);
                 await client.DisconnectAsync(true).ConfigureAwait(false);
             }
         }
 
-        public async Task SendHtmlEmailAsync(string email, string subject, string htmlMessage)
+        private static void SetCarbonCopy(EmailSenderConfiguration configuration, MimeMessage emailMessage)
         {
+            foreach (var ccRecipient in configuration.To.CarbonCopyRecipients)
+            {
+                emailMessage.Cc.Add(new MailboxAddress(ccRecipient.Value, ccRecipient.Key));
+            }
 
+            foreach (var bccRecipient in configuration.To.BlindCarbonCopyRecipients)
+            {
+                emailMessage.Bcc.Add(new MailboxAddress(bccRecipient.Value, bccRecipient.Key));
+            }
+        }
+
+        private static bool SetRecipients(EmailSenderConfiguration configuration, MimeMessage emailMessage)
+        {
+            var hasRecipients = false;
+            if (configuration.To.Recipients.Any())
+            {
+                foreach (var recipient in configuration.To.Recipients)
+                {
+                    emailMessage.To.Add(new MailboxAddress(recipient.Value, recipient.Key));
+                }
+
+                hasRecipients = true;
+            }
+
+            if (!string.IsNullOrWhiteSpace(configuration.To.EmailAddress))
+            {
+                emailMessage.To.Add(new MailboxAddress(configuration.To.Name, configuration.To.EmailAddress));
+                hasRecipients = true;
+            }
+
+            return hasRecipients;
         }
     }
 }
---
+++ proj/DevMarketplace/src/BusinessLogic/Facade/EmailSenderConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using MailKit.Security;
+using MimeKit.Text;
+
+namespace BusinessLogic.Facade
+{
+    public class FromConfiguration
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string EmailAddress { get; set; }
+    }
+
+    public class ToConfiguration : FromConfiguration
+    {
+        public ToConfiguration()
+        {
+            Recipients = new Dictionary<string, string>();
+            CarbonCopyRecipients = new Dictionary<string, string>();
+            BlindCarbonCopyRecipients = new Dictionary<string, string>();
+        }
+
+        public string Subject { get; set; }
+
+        public Dictionary<string, string> Recipients { get; }
+
+        public Dictionary<string, string> BlindCarbonCopyRecipients { get; set; }
+
+        public Dictionary<string, string> CarbonCopyRecipients { get; set; }
+    }
+
+    public class EmailSenderConfiguration
+    {
+        public FromConfiguration From { get; set; }
+
+        public ToConfiguration To { get; set; }
+
+        public TextFormat EmailFormat { get; set; }
+
+        public string EmailBody { get; set; }
+
+        public int SmtpPort { get; set; } = 25;
+
+        public string Domain { get; set; }
+
+        public string SmtpServer { get; set; }
+
+        public SecureSocketOptions SecureSocketOption { get; set; }
+    }
+}
