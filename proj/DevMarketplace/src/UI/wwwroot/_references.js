b956f33101f7e949ffe2b9622b2b12e9e1b730a8
Tosho Toshev <tosho.toshev@emerson.com>
2016-11-08 15:52:48 -0500

    Added Sass support to the project

--- proj/DevMarketplace/src/UI/wwwroot/_references.js
+++ proj/DevMarketplace/src/UI/wwwroot/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
