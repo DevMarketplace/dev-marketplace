f36018bcceae9d2c626a76ee42ef475a345976a6
Tosho Toshev <tosho.toshev@emerson.com>
2016-11-17 17:24:07 -0500

    Layout styling changes

--- proj/DevMarketplace/src/UI/wwwroot/js/site.js
+++ proj/DevMarketplace/src/UI/wwwroot/js/site.js
@@ -1,11 +1,24 @@
 ﻿(function ($) {
     var operations = {
         init: function () {
-
+            //Init functions for different 3rd party libraries
         },
 
         documentReadySetup: function () {
 
+            if ($.fn.sideNav !== 'undefined') {
+                $(".button-collapse").sideNav();
+            }
+
+            $(window).on("scroll", function () {
+                if ($(this).scrollTop() > $(".jumbotron").height()) {
+                    $(".navigation-bar").addClass("navbar-fixed");
+                    $(".navigation-bar > nav").css({ top: 0 });
+                } else {
+                    $(".navigation-bar > nav").css({ top: $(".jumbotron").height() });
+                    $(".navigation-bar").removeClass("navbar-fixed");
+                }
+            });
         }
     };
 
