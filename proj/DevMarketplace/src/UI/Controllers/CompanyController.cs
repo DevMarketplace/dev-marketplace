a37ba242599a49b0337317aee6aab755296f41ab
Tosho Toshev <tosho_az@yahoo.com>
2016-12-12 23:23:42 -0500

    Added a Company controller

---
+++ proj/DevMarketplace/src/UI/Controllers/CompanyController.cs
@@ -0,0 +1,48 @@
+﻿#region License
+// The Developer Marketplace is a web application that allows individuals, 
+// teams and companies share KanBan stories, cards, tasks and items from 
+// their KanBan boards and Scrum boards. 
+// All shared stories become available on the Developer Marketplace website
+//  and software engineers from all over the world can work on these stories. 
+// 
+// Copyright (C) 2016 Tosho Toshev
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// GitHub repository: https://github.com/cracker4o/dev-marketplace
+// e-mail: cracker4o@gmail.com
+#endregion
+using System;
+using DataAccess.Entity;
+using DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class CompanyController
+    {
+        private readonly IGenericRepository<Company> _companyRepository;
+
+        public CompanyController(IGenericRepository<Company> companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ proj/DevMarketplace/test/UITests/Controllers/CompanyControllerShould.cs
@@ -0,0 +1,57 @@
+﻿#region License
+// The Developer Marketplace is a web application that allows individuals, 
+// teams and companies share KanBan stories, cards, tasks and items from 
+// their KanBan boards and Scrum boards. 
+// All shared stories become available on the Developer Marketplace website
+//  and software engineers from all over the world can work on these stories. 
+// 
+// Copyright (C) 2016 Tosho Toshev
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// GitHub repository: https://github.com/cracker4o/dev-marketplace
+// e-mail: cracker4o@gmail.com
+#endregion
+using DataAccess.Entity;
+using DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using UI.Controllers;
+
+namespace UITests.Controllers
+{
+    [TestFixture]
+    public class CompanyControllerShould
+    {
+        private CompanyController _controller;
+        private IGenericRepository<Company> _companyRepositoryMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _companyRepositoryMock = Mock.Of<IGenericRepository<Company>>();
+        }
+
+        [Test]
+        public void DoGetTheCreateCompanyPage()
+        {
+            //Act
+            IActionResult result = _controller.Create();
+
+            //Assert
+            
+        }
+    }
+}
