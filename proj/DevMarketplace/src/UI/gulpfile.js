61291310697f42dd578144cb7cb36f3ba6d45c14
Tosho Toshev <tosho_az@yahoo.com>
2017-01-18 22:33:53 -0500

    gulp modifications

--- proj/DevMarketplace/src/UI/gulpfile.js
+++ proj/DevMarketplace/src/UI/gulpfile.js
@@ -2,19 +2,29 @@
 "use strict";
 
 var gulp = require("gulp"),
+    gutil = require('gulp-util'),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
     fs = require("fs"),
+    path = require("path"),
     sass = require("gulp-sass"),
-    watchify = require('watchify'),
-    browserify = require('browserify');
+    watchify = require("watchify"),
+    aliasify = require("aliasify"),
+    browserify = require("browserify");
 
 var paths = {
     webroot: "./wwwroot/",
     nodeModules: "./node_modules/"
 };
+
+var aliasifyConfig = {
+    aliases: {
+        "vue$": "vue/dist/vue.js"
+    },
+    verbose: true
+}
 
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
@@ -23,6 +33,77 @@ paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
+var jsPaths = [
+    process.env.INIT_CWD + "\\App\\shared",
+];
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+      .filter(function (file) {
+          return fs.statSync(path.join(dir, file)).isDirectory();
+      });
+}
+
+function compileJS(input, output) {
+    // set up the browserify instance on a task basis
+    var b = browserify({
+        debug: true,
+        entries: [input],
+        basedir: process.env.INIT_CWD,
+        paths: jsPaths
+    });
+
+    return b.transform(aliasify, aliasifyConfig)
+        .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+          .on("error", gutil.log)
+      .pipe(gulp.dest(output));
+}
+
+function watchFolder(input, output) {
+    var b = browserify({
+        entries: [input],
+        cache: {},
+        packageCache: {},
+        plugin: [watchify],
+        basedir: process.env.INIT_CWD,
+        paths: jsPaths
+    });
+
+    function bundle() {
+        b.transform(aliasify, aliasifyConfig)
+            .bundle()
+            .pipe(source("bundle.js"))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .on("error", gutil.log)
+            .pipe(sourcemaps.write("./"))
+            .pipe(gulp.dest(output));
+
+        gutil.log("Bundle rebuilt!");
+    }
+    b.on("update", bundle);
+    bundle();
+}
+
+gulp.task("build-vue:js", function () {
+    var folders = getFolders("App");
+    gutil.log(folders);
+    folders.map(function (folder) {
+        compileJS("App//" + folder + "//main.js", "Scripts//app//" + folder);
+    });
+});
+
+gulp.task("default", function () {
+    var folders = getFolders("App");
+    gutil.log(folders);
+    folders.map(function (folder) {
+        watchFolder("App//" + folder + "//main.js", "Scripts//app//" + folder);
+    });
+
+});
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
