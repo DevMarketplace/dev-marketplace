0a643a7c651171ac09c2a4d5df778be0263030e1
Tosho Toshev <tosho.toshev@emerson.com>
2017-04-13 12:41:34 -0400

    Fixed the styling for the modal popup

--- proj/DevMarketplace/src/UI/wwwroot/js/register.js
+++ proj/DevMarketplace/src/UI/wwwroot/js/register.js
@@ -1,5 +1,6 @@
 ﻿(function ($) {
     $(document).on("organizationCreated", function (evt, orgInfo) {
-        $("#CompanyId").append("<option value="+ orgInfo.companyId +">"+ orgInfo.name +"</option>");
+        $("#CompanyId option:selected").remove();
+        $("#CompanyId").append("<option selected value="+ orgInfo.companyId +">"+ orgInfo.name +"</option>");
     });
 })(jQuery)
