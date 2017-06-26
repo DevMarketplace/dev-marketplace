bc01965b85f21c4aa64882a6a1d4d9306217e872
Tosho Toshev <tosho.toshev@emerson.com>
2017-06-26 12:59:12 -0400

    Removed all unnecessary Javascript files that are usually generated by TypeScript

--- src/UI/wwwroot/app/models/organization.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Organization = (function () {
-    function Organization() {
-    }
-    return Organization;
-}());
-exports.Organization = Organization;
-//# sourceMappingURL=organization.model.js.map
