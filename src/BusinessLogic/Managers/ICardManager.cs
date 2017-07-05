768783231420fe9881fd434d9045f7330020ed66
Tosho Toshev <tosho_az@yahoo.com>
2017-07-04 22:39:21 -0400

    Added a new CardManager interface and related business objects

---
+++ src/BusinessLogic/Managers/ICardManager.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLogic.BusinessObjects;
+using System;
+
+namespace BusinessLogic.Managers
+{
+    public interface ICardManager
+    {
+        /// <summary>
+        /// Creates a new task for a certain Card
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        Guid CreateCardTask(TaskBo task);
+
+        /// <summary>
+        /// Updates an existing task
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        TaskBo UpdateCardTask(TaskBo task);
+
+
+        /// <summary>
+        /// Deletes a task by Id
+        /// </summary>
+        /// <param name="id"></param>
+        void DeleteCardTask(Guid id);
+
+        /// <summary>
+        /// Creates a new card
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
+        Guid Create(CardBo card);
+
+        /// <summary>
+        /// Updates an existing card
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
+        CardBo Update(CardBo card);
+
+        /// <summary>
+        /// Deletes an existing card
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(Guid id);
+    }
+}
